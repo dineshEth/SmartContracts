f8b5fed3e82cce53b9bafd53b2494a3d0a192e08
Dinesh <dineshchauhan416pd@gmail.com>
2023-02-15 01:09:35 +0530

    whitelisting address smart contract updated

--- WhiteList.sol
+++ WhiteList.sol
@@ -6,10 +6,20 @@ contract WhiteList{
      uint8 maxWhitlistedAddresses; // max addressescan be white listed
      uint8 numOfWhiteListAddresses; // number of whitelisted initially equals to 0
 
+     address onwer;
+
      mapping(address => bool ) whitelistAddress;  // addreess which are white listed
+
+
+     // modidifier 
+     modifier onlyOnwer (){
+          require(msg.sender==onwer);
+          _;
+     }
 
      constructor (uint8 _maxWhiteListAddresses) {
           maxWhitlistedAddresses = _maxWhiteListAddresses;
+          onwer = msg.sender;
      }
 
      function whiteList() public {
@@ -18,4 +28,13 @@ contract WhiteList{
           whitelistAddress[msg.sender] = true;  // whitelist true
           numOfWhiteListAddresses++; // increment
      }
+
+     function increseWhitelistaddres(uint8 _moreAddress) public onlyOnwer {
+          // only onwer (deployer of the smart contract) can call the funciton and increment
+          maxWhitlistedAddresses += _moreAddress; // more address can be whitelisted 
+     }
 }
+
+// OTHER FUNCTIONALITIES 
+// owner can remove address from white listed in such case
+// onwer can also white list address if need
